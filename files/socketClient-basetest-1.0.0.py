35a1e4ab4d8e5480894977cee2892492970f8dca
Strawberrydog32 <66707616+Strawberrydog32@users.noreply.github.com>
2021-05-18 13:15:03 -0700

    Update socketClient-basetest-1.0.0.py

--- files/socketClient-basetest-1.0.0.py
+++ files/socketClient-basetest-1.0.0.py
@@ -1,3 +1,4 @@
 import socket 
 import time
-print("socket socketHost-basetest-1.0.0 up and running, no content yet") time.sleep(1)
+print("socket socketHost-basetest-1.0.0 up and running, no content yet") 
+time.sleep(1)
